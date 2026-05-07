331fee47611c2d5b2e7575fd507c2694ce4f7540
Daniel Jansen Harianto <111278708+danieljharianto@users.noreply.github.com>
2026-05-06 21:15:55 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/PX_WebMap/',
+  base: '/', // penting untuk Vercel
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: { dashboard: 'dashboard.html' }
+      input: {
+        index: 'index.html',
+        dashboard: 'dashboard.html'
+      }
     }
   }
 })
